19005019144756bb7e52bf9fe4b0325ff721cdee
Sissillie <sydcshell@gmail.com>
2026-08-02 20:09:58 -0400

    E: Ch2: L3: HTMLNode

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,27 +1,34 @@
 import unittest
+
 from textnode import TextNode, TextType
 
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
-        node = TextNode("This is a text node", TextType.BOLD)
+        node = TextNode("This is a text node", TextType.TEXT)
+        node2 = TextNode("This is a text node", TextType.TEXT)
+        self.assertEqual(node, node2)
+
+    def test_eq_false(self):
+        node = TextNode("This is a text node", TextType.TEXT)
         node2 = TextNode("This is a text node", TextType.BOLD)
-        self.assertEqual(node, node2)
-
-    def test_link_match(self):
-        node = TestTextNode("This is a test node", TextType.LINK, "www.google.com")
-        node2 = TestTextNode("This isn't a test node", TextType.LINK, "www.kink.com")
         self.assertNotEqual(node, node2)
 
-    def test_link_none(self):
-        node = TestTextNode("Rotten Mango", TextType.ITALIC)
-        node2 = TestTextNode("Rotten Mango", TextType.ITALIC)
+    def test_eq_false2(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        node2 = TextNode("This is a text node2", TextType.TEXT)
+        self.assertNotEqual(node, node2)
+
+    def test_eq_url(self):
+        node = TextNode("This is a text node", TextType.ITALIC, "https://www.boot.dev")
+        node2 = TextNode("This is a text node", TextType.ITALIC, "https://www.boot.dev")
         self.assertEqual(node, node2)
 
-    def test_text_type(self):
-        node = TestTextNode("Aang is the best Avatar!", TextType.BOLD)
-        node2 = TestTextNode("Katara is the best Avatar!", TextType.ITALIC)
-        self.assertNotEqual(node, node2)
+    def test_repr(self):
+        node = TextNode("This is a text node", TextType.TEXT, "https://www.boot.dev")
+        self.assertEqual(
+            "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
+        )
 
 
 if __name__ == "__main__":
