ecb957473fb11717f7b9eeb42d7bdb2ae84d46ec
Sissillie <sydcshell@gmail.com>
2026-08-05 13:06:46 -0400

    K: Ch5: L3 & 4 More Pages

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -28,9 +28,9 @@ def markdown_to_blocks(markdown: str) -> list[str]:
 def block_to_block_type(block: str) -> BlockType:
     lines = block.split("\n")
 
-    if block.startswith(("# ", "## ", "### ", "#### ", "##### ")):
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
         return BlockType.HEADING
-    if len(lines) > 1 and lines[0].startswith(("```")) and lines[-1].startswith(("```")):
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
         return BlockType.CODE
     if block.startswith(">"):
         for line in lines:
@@ -149,4 +149,3 @@ def quote_to_html_node(block: str) -> ParentNode:
     content = " ".join(new_lines)
     children = text_to_children(content)
     return ParentNode("blockquote", children)
-                    
